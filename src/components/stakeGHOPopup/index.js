5ca910bb49843e5e324c2bb14cff139cd5809c75
Anil Dukkipatty <anildukkipatty@gmail.com>
2024-01-18 18:03:35 +0530

    modified the stakeGHOPopup contents

--- src/components/stakeGHOPopup/index.js
+++ src/components/stakeGHOPopup/index.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Button,
   PopUpContainer,
   PopUpWrapper,
-  CommonWrapper,
-  ProgressBarContainer,
-  FirstSegment,
-  SecondSegment,
-  ThirdSegment,
+  // CommonWrapper,
+  // ProgressBarContainer,
+  // FirstSegment,
+  // SecondSegment,
+  // ThirdSegment,
   CheckboxLabel,
   CustomCheckbox,
   Warning
@@ -15,11 +15,15 @@ import {
 
 // import { GRADIENT_TEXT_COLOR_GREEN } from "src/assets/variables/variables";
 
-import warningIcon from "src/assets/images/warning.svg";
+// import warningIcon from "src/assets/images/warning.svg";
 
 const StakeGHOPopup = ({ popRef }) => {
   const [isChecked, setIsChecked] = useState(false);
   const [warning, setWarning] = useState(false);
+
+  useEffect(() => {
+    // read user's GHO balance
+  }, [])
 
   const handleCheckboxChange = () => {
     setIsChecked(!isChecked);
@@ -28,25 +32,20 @@ const StakeGHOPopup = ({ popRef }) => {
   return (
     <PopUpWrapper ref={popRef}>
       <div className="profile"></div>
-      <h3>DApp Name</h3>
+      <h3>Stake GHO</h3>
       <p style={{ fontWeight: "500", marginTop: "14px" }}>
-        DApp name supports MATIC token
+        Get credit against the staked GHO
       </p>
       <PopUpContainer $warning={warning ? "#979797" : ""}>
         <div className="wrapper">
-          <p>You’ll get</p>
-          <h3>11.8022 MATIC</h3>
+          <p>Choose how much to Stake</p>
+          <input style={{marginTop: '10px', marginBottom: '10px'}} type="number" />
           <p>
-            Worth <span>10 GHO</span>
+            You curently own <span>10 GHO</span>
           </p>
         </div>
-        <div className="wrapper">
-          <p>Your credit limit will be affected</p>
-          <ProgressBarContainer>
-            <FirstSegment />
-            <SecondSegment />
-            <ThirdSegment />
-          </ProgressBarContainer>
+        {/* <div className="wrapper">
+          
           <CommonWrapper $justifyContent="space-between">
             <p className="creditLimit claimed">56% Claimed</p>
             <p className="creditLimit added">25% Added</p>
@@ -54,17 +53,17 @@ const StakeGHOPopup = ({ popRef }) => {
               {warning && <img src={warningIcon} />}19% Remaining
             </p>
           </CommonWrapper>
-        </div>
-        <div className="wrapper">
+        </div> */}
+        {/* <div className="wrapper">
           <p>you have to Payback in</p>
           <h3>3 days</h3>
-        </div>
+        </div> */}
       </PopUpContainer>
 
       {!warning ? (
         <CheckboxLabel>
           <CustomCheckbox checked={isChecked} onChange={handleCheckboxChange} />I
-          agree to repay the specified amount before the due date to avoid penalties.
+          agree to the terms and conditions.
         </CheckboxLabel>
       ) : (
         <Warning>
@@ -75,7 +74,7 @@ const StakeGHOPopup = ({ popRef }) => {
         </Warning>
       )}
 
-      <Button>{!warning ? "CLAIM NOW" : "STAKE GHO"}</Button>
+      <Button>Stake GHO</Button>
     </PopUpWrapper>
   );
 };
